1895b635ad39c307fae1793bf76d58454e5b3893
gggerman <21german.garcia@gmail.com>
2022-02-07 22:49:19 -0300

    Fetching data from Node with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import { Room, Star } from '@mui/icons-material';
+import axios from 'axios';
+import { format } from 'timeago.js';
 import './App.css';
 
 function App() {
+  const currentUser = "Germán";
+
+  const [pins, setPins] = useState([]);
+
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -13,6 +21,23 @@ function App() {
     zoom: 4
   });
 
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const response = await axios.get("/pins");
+        setPins(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getPins();
+  }, [])
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  }
+
   return (
     <div className="App">
       <ReactMapGL
@@ -20,37 +45,48 @@ function App() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={nextViewport => setViewport(nextViewport)}
       >
-        <Marker
-          latitude={48.858093} 
-          longitude={2.294694} 
-          offsetLeft={-20} 
-          offsetTop={-10}>
-          <Room style={{fontSize: viewport.zoom * 7, color: "slateblue"}}/>
-        </Marker>
-        <Popup
-          latitude={48.858093}
-          longitude={2.294694}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="left" >
-          <div className="card">
-            <label>Place</label>
-            <h4 className="place">Eiffel Tower</h4>
-            <label>Review</label>
-            <p className="description">Beautiful place</p>
-            <label>Rating</label>
-            <div className="stars">  
-              <Star className="star" />
-              <Star className="star" />
-              <Star className="star" />
-              <Star className="star" />
-              <Star className="star" />
-            </div>
-            <label>Information</label>
-            <span className="username">Created By <b>Germancete</b></span>
-            <span className="date">1 hour ago</span>
-          </div>
-        </Popup>
+        {pins.map((pin, index) => (
+          <>
+            <Marker
+              latitude={pin.latitude} 
+              longitude={pin.longitude} 
+              offsetLeft={-20} 
+              offsetTop={-10}>
+              <Room 
+                style={{fontSize: viewport.zoom * 7, color: pin.username === currentUser ? "tomato" : "slateblue", cursor: "pointer"}}
+                onClick={() => handleMarkerClick(pin._id)}/>
+            </Marker>
+            {pin._id === currentPlaceId && (
+              <Popup
+                latitude={pin.latitude}
+                longitude={pin.longitude}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{pin.title}</h4>
+                  <label>Review</label>
+                  <p className="description">{pin.description}</p>
+                  <label>Rating</label>
+                  <div className="stars">  
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                  </div>
+                  <label>Information</label>
+                  <span className="username">Created By <b>{pin.username}</b></span>
+                  <span className="date">{format(pin.createdAt)}</span>
+                </div>
+              </Popup>
+              )
+            }
+            </>
+          ))}
       </ReactMapGL>
     </div>
   );
